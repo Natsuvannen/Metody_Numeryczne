02d440789d1d3434495ea4614e91899c3c0ca9cd
unknown <242367@student.pwr.edu.pl>
2019-03-06 17:54:27 +0100

    Lab1

--- Lab01.py
+++ Lab01.py
@@ -4,38 +4,43 @@ from matplotlib.pyplot import *
 
 #zadanie 1
 
-#x=linspace(56, 100, 45)
-#y = 2*x**2 + 2*x + 2
-#print(y)
+x=arange(56, 101, 1)
+y = 2*x**2 + 2*x + 2
+print(y)
 
 #zadanie 2
 
-#print("Podaj liczbę ...")
-#x01=int(input())
-#for i in range(x01+1):
-    #if i==0:
-        #y01=1
-    #else:
-        #y01 = y01*i
-#print(y01)
+print("Podaj liczbę ...")
+x01=int(input())
+for i in range(x01+1):
+    if i==0:
+        y01=1
+    else:
+        y01 = y01*i
+print(y01)
 
 #zadanie 3
 
-#lista=[9, 0, 5, 7, 2]
-#k=lista[0]
-#for i in range(len(lista)):
-    #if k>lista[i]:
-        #k=lista[i]
-
-#print(lista.index(k), k)
+print("Podaj rozmiar listy ...")
+n=int(input())
+lista=[]
+for i in range(n):
+    print("Podaj wartość")
+    lista.append(int(input()))
+k=lista[0]
+for i in range(len(lista)):
+    if k>lista[i]:
+        k=lista[i]
+print("Index : ", lista.index(k),"Wartość" , k)
 
 #zadanie 4
 
+print("Podaj długość dziedziny ...")
 x04 = int(input())
-x_values = zeros(x04+1)
-y_values = zeros(x04+1)
+x_values = zeros(x04)
+y_values = zeros(x04)
 
-for i in range(x04+1):
+for i in range(x04):
     x_values[i] = i
     y_values[i] = tan(x_values[i]**3-x_values[i]*42)
 
