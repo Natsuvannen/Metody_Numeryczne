60bbd92c47a4463bfc0d164ef12d5506fc16788b
242367 <242367@student.pwr.edu.pl>
2019-03-06 10:24:52 +0100

    First lab

---
+++ Lab01.py
@@ -0,0 +1,30 @@
+from numpy import *
+#zadanie 1
+
+#x=linspace(56, 100, 45)
+#y = 2*x**2 + 2*x + 2
+#print(y)
+
+#zadanie 2
+
+#print("Podaj liczbę ...")
+#x01=int(input())
+#for i in range(x01+1):
+    #if i==0:
+        #y01=1
+    #else:
+        #y01 = y01*i
+#print(y01)
+
+#zadanie 3
+
+#lista=[9, 0, 5, 7, 2]
+#k=lista[0]
+#for i in range(len(lista)):
+    #if k>lista[i]:
+        #k=lista[i]
+
+#print(lista.index(k), k)
+
+#zadanie 4
+
